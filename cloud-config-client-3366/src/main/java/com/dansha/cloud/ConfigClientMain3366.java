7909b4bfb4185605927f2d3f4f0a572f22aa17c8
liuxiaokang <liuxiaokang@bus365.com>
2020-06-30 16:27:55 +0800

    online-bus完成

---
+++ cloud-config-client-3366/src/main/java/com/dansha/cloud/ConfigClientMain3366.java
@@ -0,0 +1,14 @@
+package com.dansha.cloud;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+/**
+ * @date 2020/3/15 22:14
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
